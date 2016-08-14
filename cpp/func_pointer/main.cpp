8eecd7cc93a7390948ef6522c10da780aa72b071
Leiming Yu <leimingyu830@gmail.com>
2016-08-14 14:40:43 -0400

    stl

--- cpp/func_pointer/main.cpp
+++ cpp/func_pointer/main.cpp
@@ -1,24 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void fun1() {
+void fun1(float *x) {
 	printf("this is func 1!\n");
 }
 
-void fun2() {
+void fun2(float *x) {
 	printf("this is func 2!\n");
 }
 
-void fun3() {
+void fun3(float *x) {
 	printf("this is func 3!\n");
 }
 
-void (*func_ptr[3])()= {fun1, fun2, fun3};
+void (*func_ptr[3])(float *x)= {fun1, fun2, fun3};
 
 int main() {
-	func_ptr[0]();	
-	func_ptr[2]();	
-	func_ptr[1]();	
+	float x = 1.1f;
+	func_ptr[0](&x);	
+	func_ptr[2](&x);	
+	func_ptr[1](&x);	
 
 	return 0;
 }
