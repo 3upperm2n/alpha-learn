891f144f78a1b0d2be65a0b56ab658cd7932f8fe
Leiming Yu <leimingyu830@gmail.com>
2017-12-13 15:24:27 -0500

    add dict to the list

--- python/multiprocessing/list_of_dict.py
+++ python/multiprocessing/list_of_dict.py
@@ -7,6 +7,8 @@ manager = Manager()
 # create a list proxy and append a mutable object (a dictionary)
 lproxy = manager.list()
 lproxy.append({})
+print lproxy[0]
+
 # now mutate the dictionary
 d = lproxy[0]
 d['a'] = 1
@@ -16,3 +18,33 @@ d['b'] = 2
 lproxy[0] = d
 
 print lproxy[0]
+
+
+
+
+
+manyproxy = [manager.list() for i in xrange(2)]
+print manyproxy
+print manyproxy[0]
+
+#for item in manyproxy:
+#    item = item.append({})
+
+for i in xrange(len(manyproxy)):
+    lp = manyproxy[i]
+    lp.append({})
+    #manyproxy[i] = manyproxy[i].append({})
+print manyproxy
+print manyproxy[0]
+
+
+
+for i in xrange(len(manyproxy)):
+    d = manyproxy[i][0]
+    d['yu'] = 1 + i 
+    d['chen'] = 2 + i
+    manyproxy[i] = d
+
+print manyproxy
+print manyproxy[0]
+print manyproxy[1]
